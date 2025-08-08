033eb8d66f56d01c5d20c74fa6a55ebec68e7b47
Prateek Rohilla <prateek98rohilla@gmail.com>
2025-08-08 19:21:27 +0530

    CSS DOM properties

--- DOM/cssManupulation.js
+++ DOM/cssManupulation.js
@@ -1,5 +1,9 @@
 const box = document.getElementById('box');
+const box2 = document.getElementById('box2');
 const btn = document.getElementById('toggleBtn');
+const themeBtn = document.getElementById('themeBtn');
+const addBtn = document.getElementById('uline');
+const removeBtn = document.getElementById('rmUline');
 
 btn.addEventListener('click',() => {
     if(box.style.backgroundColor === 'lightblue')
@@ -7,3 +11,14 @@ btn.addEventListener('click',() => {
     else
         box.style.backgroundColor = 'lightblue';
 });
+
+themeBtn.addEventListener('click', ()=>{
+    box2.classList.toggle('dark-mode');
+});
+
+addBtn.addEventListener('click',()=>{
+    box2.classList.add('decorate');
+})
+removeBtn.addEventListener('click',()=>{
+    box2.classList.remove('decorate');
+})
