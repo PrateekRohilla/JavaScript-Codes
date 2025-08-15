f7e3134cb804fd62793b0067fc1e8dd4432089e9
Prateek Rohilla <prateek98rohilla@gmail.com>
2025-08-15 12:25:15 +0530

    optimal - use set

--- NamasteDev JS Problems/removeDuplicates.js
+++ NamasteDev JS Problems/removeDuplicates.js
@@ -27,7 +27,25 @@ function removeDuplicates(arr){
     return result;
 }
 
+//optimal - use set
+function removeDuplicates2(arr){
+    
+    const seen = new Set();
+    const result = [];
+
+    for(const item of arr){
+        if(!seen.has(item)){
+            seen.add(item);
+            result.push(item);
+        }
+    }
+
+    return result;
+}
+
+
+
 //let A = [1,"1",1];
 let A = [1, 23, null, 4, 5, undefined, 12, 1, null, 23, undefined];
-let res = removeDuplicates(A)
+let res = removeDuplicates2(A)
 console.log(res);
