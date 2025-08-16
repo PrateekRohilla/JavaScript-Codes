9664232597dac9ae5c9ee2e177f099f376c04967
Prateek Rohilla <prateek98rohilla@gmail.com>
2025-08-16 23:22:25 +0530

    OP using slice

--- NamasteDev JS Problems/arrayChunking.js
+++ NamasteDev JS Problems/arrayChunking.js
@@ -1,30 +1,40 @@
-let arr = [1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 let n = 2;
 
-function chunkArray(arr,n){
+function chunkArray(arr, n) {
     let result = [];
 
-    if(arr.length == 0)
+    if (arr.length == 0)
         return result;
 
     let index = 0
 
-    while(index < arr.length){
-        let tempArr = [];        
+    while (index < arr.length) {
+        let tempArr = [];
 
-        for(let i=1;i<=n;i++){
+        for (let i = 1; i <= n; i++) {
             tempArr.push(arr[index]);
             index++;
-        
-            if(index>=arr.length)
+
+            if (index >= arr.length)
                 break;
         }
 
         result.push(tempArr);
     }
-    
+
     return result;
 }
 
-let resultArr = chunkArray(arr,n);
+//optimal
+function chunkedOP(arr, n) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i += n)
+        result.push(arr.slice(i, i + n));
+
+    return result;
+}
+
+let resultArr = chunkedOP(arr, n);
 console.log(resultArr);
