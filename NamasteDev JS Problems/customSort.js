d4258960f1d6b64fdc2e9540bb154ec5f21903f0
Prateek Rohilla <prateek98rohilla@gmail.com>
2025-08-18 11:34:01 +0530

    edge case

--- NamasteDev JS Problems/customSort.js
+++ NamasteDev JS Problems/customSort.js
@@ -1,11 +1,17 @@
 let arr = ['g', 3, 4, 'h', 5, 'j', 'g', 2, 3, 'X', 'N', 0, 'S', 'H', 'A', 'S', 'D', 5, 'h', 'j', 23, 'h', 'j'];
+//let arr = ['d','f','k','K','A','S','J','A','g'];
+//let arr = [4,3,6,8,6,7,8,1,2,0];
+//let arr = [1,'s'];
+//let arr = [2];
+//let arr = [];
+
 
 function customSort(arr) {
 
     let sorted = [];
 
     if (arr.length <= 1) {
-        sorted[0] = arr[0];
+        sorted = [...arr];
         return sorted;
     }
 
@@ -35,6 +41,4 @@ function customSort(arr) {
 }
 
 let sortedArr = customSort(arr);
-
-
 console.log(sortedArr);
