54196bdc7cf1ec3400e39bdc2f053be7cdf1fb90
Prateek Rohilla <prateek98rohilla@gmail.com>
2025-08-20 13:04:59 +0530

    val conv to str

---
+++ NamasteDev JS Problems/largeNumberForm.js
@@ -0,0 +1,13 @@
+function largeNumberForm(arr){
+
+    let result;
+    let stringArr = [];
+
+    for(let i=0;i<arr.length;i++)
+        stringArr [i] = arr[i].toString();
+
+    console.log(result);
+}
+
+let arr = [3,30,34,5,9];
+largeNumberForm(arr);
