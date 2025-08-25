cb02c3e3bf7b2b63788fcbcf909e4a2c8bf96142
Prateek Rohilla <prateek98rohilla@gmail.com>
2025-08-25 20:29:47 +0530

    two ptr sol

--- NamasteDev JS Problems/reverseWords.js
+++ NamasteDev JS Problems/reverseWords.js
@@ -1,3 +1,7 @@
+//LC - 557 | Reverse Words in a String III
+
+//TC - O(n) | SC - O(n)
+/*
 function reverseWords(str){
 
     //get arr of words (split by space)
@@ -10,7 +14,34 @@ function reverseWords(str){
 
     return result;
 }
+*/
 
+//two pointer solution (& more language generic code)
+function reverseWords(str) {
+    
+    let chars = str.split('');
+    let start = 0;
+
+    for (let i = 0; i <= chars.length; i++) {
+
+        // when space or end of string is found -> reverse this word
+        if (i === chars.length || chars[i] === ' ') {
+            let left = start, right = i - 1;
+
+            // two pointer reversal
+            while (left < right) {
+                [chars[left], chars[right]] = [chars[right], chars[left]];
+                left++;
+                right--;
+            }
+
+            // move start to next word
+            start = i + 1; 
+        }
+    }
+
+    return chars.join('');
+}
 
 
 let str = "what about you";
