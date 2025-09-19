3a53897b06d7eee8c4ace87b8cedd833906dd086
Prateek Rohilla <prateek98rohilla@gmail.com>
2025-09-19 23:59:29 +0530

    groupBy inbuilt

--- NamasteDev JS Problems/groupBy.js
+++ NamasteDev JS Problems/groupBy.js
@@ -15,11 +15,32 @@ function groupBy(arr, key) {
     return result;
 }
 
+
 let users = [
     { name: 'Alice', age: 25 },
     { name: 'Bob', age: 30 },
-    { name: 'Charlie', age: 25 }
+    { name: 'Charlie', age: 25 },
+    { name: 'ram', age: 18 },
+    { name: 'shyam', age: 20 },
+    { name: 'ghanshyam', age: 15 },
+    { name: 'prateek', age: 26 }
 ];
 
 let res = groupBy(users, 'age');
 console.log(res);
+
+
+//groupBy native js function for objects
+//groupBy(arr, fn) | fn will run for every object in array
+const groupByage = Object.groupBy(users, user => user.age);
+console.log(groupByage);
+
+//get users with only age 25
+console.log(groupByage[25]);
+
+//can also apply logic & get data
+const maturedGuys = Object.groupBy(users, 
+    user => user.age >= 18 ? "Mature" : "Immature"
+);
+
+console.log(maturedGuys);
