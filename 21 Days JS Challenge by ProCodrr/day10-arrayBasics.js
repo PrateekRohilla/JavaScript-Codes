ab11bd48b84cc4fe5b41121a90e42e5ae64c6392
Prateek Rohilla <prateek98rohilla@gmail.com>
2026-01-04 16:24:18 +0530

    day10 assignment basic

--- 21 Days JS Challenge by ProCodrr/day10-arrayBasics.js
+++ 21 Days JS Challenge by ProCodrr/day10-arrayBasics.js
@@ -33,10 +33,64 @@ function revArr(arr){
 }
 
 
+//second largest
+function secondLarge(arr){
+
+    let max = -1;
+    let secondMax = -1
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] > max)
+            max = arr[i]
+
+        if(arr[i] < max && arr[i] > secondMax)
+            secondMax = arr[i];
+    }
+
+    return secondMax;
+}
+
+
+//check if two arrays are equal
+function checkEqual(arr,arr2){
+
+    if(arr.length !== arr2.length)
+        return 'not equal';
+
+    for(let i=0;i<arr.length;i++)
+        if(arr[i] !== arr2[i])
+            return 'not equal';
+
+    return 'equal';
+}
+
+
+//count even & odd numbers
+function evenOdd(arr){
+
+    let even = 0;
+    let odd = 0;
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    return [even,odd];
+}
+
+
 
 
 
 let arr = [3,7,1,5,2];
+let arr2 = [1,5,2,7,3];
+let arr3 = [3,7,1,5,2];
 
 //console.log(minMax(arr));
 //console.log(revArr(arr));
+//console.log(secondLarge(arr));
+//console.log(checkEqual(arr,arr3));
+//console.log(evenOdd(arr));
