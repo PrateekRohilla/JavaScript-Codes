4bcd340b76ab44734c833b544e30748a9963fbb7
Prateek Rohilla <prateek98rohilla@gmail.com>
2026-01-22 23:13:03 +0530

    ex-tailRecursion

--- 21 Days JS Challenge by ProCodrr/day17-recursion.js
+++ 21 Days JS Challenge by ProCodrr/day17-recursion.js
@@ -9,8 +9,19 @@ function print(n){
     console.log(n);
 }
 
+//tail recursion
+function print2(n){
+
+    if(n == 0)
+        return;
+    
+    console.log(n);
+
+    print2(n-1);
+}
 
 
 
 
-console.log(print(5));
+
+console.log(print2(5));
