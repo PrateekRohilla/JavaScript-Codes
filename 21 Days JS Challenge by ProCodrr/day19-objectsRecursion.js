500e45c3d7a810dae1f055a45f5767da51a616c9
Prateek Rohilla <prateek98rohilla@gmail.com>
2026-02-06 20:33:33 +0530

    nested obj non rec version

---
+++ 21 Days JS Challenge by ProCodrr/day19-objectsRecursion.js
@@ -0,0 +1,55 @@
+//print all values in nested object - non recursive
+function objPrint(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'object' && obj[key] !== null)
+            objPrint(obj[key]);
+        else
+            console.log(`${key} , ${obj[key]}`);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+let obj = {
+    a: 1,
+    b: {
+        c: 2,
+        d: {
+            e: 3,
+            f: {
+                g: 'Hi'
+            }
+        }
+    }
+}
+
+let obj2 = {
+    a: 1,
+    b: [
+        { c: 3 },
+        {
+            d: {
+                e: 5,
+                f: {
+                    g: 'Hi',
+                },
+            }
+        }
+    ]
+}
+
+
+
+
+
+
+console.log(objPrint(obj2));
