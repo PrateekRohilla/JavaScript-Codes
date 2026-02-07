6edb5a8d9ce9f722d9a622f146353c754f7557e6
Prateek Rohilla <prateek98rohilla@gmail.com>
2026-02-07 23:27:36 +0530

    more on defining

--- Objects/creatingObject.js
+++ Objects/creatingObject.js
@@ -3,3 +3,14 @@ const obj = {
     key1 : value1,
     key2 : value2
 };
+
+const obj1 = {
+  property1: value1, // property name may be an identifier
+  2: value2, // or a number
+  "property n": value3, // or a string
+  //nested object
+  nestedProperty : {  
+    np1 : nestedValue,
+    np2 : nestedValue2,
+  }
+};
